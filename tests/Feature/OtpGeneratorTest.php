62f7bf86226873ea9176d32bc67fd013ca879a2b
thuraaung2493 <thuraaung2493@gmail.com>
2024-09-07 10:35:17 +0700

    fix versioning, test cases and update package

--- tests/Feature/OtpGeneratorTest.php
+++ tests/Feature/OtpGeneratorTest.php
@@ -8,7 +8,9 @@ it('can create an otp generator', function (): void {
     $generator = new OtpGenerator();
 
     expect($generator)
-        ->toBeInstanceOf(OtpGenerator::class)
+        ->toBeInstanceOf(OtpGenerator::class);
+
+    expect(OtpGenerator::class)
         ->toHaveMethod('generate')
         ->and($generator->generate())
         ->toBeString()
